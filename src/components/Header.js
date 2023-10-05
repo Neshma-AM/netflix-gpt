84da8e5da9a0f37b4682ddadfb489e944b36acdf
Neshma-AM <neshmaam99@gmail.com>
2023-10-05 17:25:20 +0530

    css updation

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
         <div className='absolute bg-gradient-to-b from-black-600 to-black-600'>
       <img className='w-[240px]' src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
       alt='logo' />
+      <p>Checking if changes are getting added</p>
         </div>
     )
 
